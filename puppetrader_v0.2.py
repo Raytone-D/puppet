2487f4a9c69e8c77395d51a5020e003e89757cd4
睿瞳深邃 <raytone@qq.com>
2017-02-14 23:05:33 +0800

    Add files via upload
    用SendMessage发消息获取持仓数据。

---
+++ puppetrader_v0.2.py
@@ -0,0 +1,153 @@
+"""puppetrader for ths client uniform"""
+__author__ =  '睿瞳深邃(https://github.com/Raytone-D)'
+__project__ = '扯线木偶(puppetrader for ths client unity)'
+__version__ = "0.2"
+'推荐使用：anaconda3 最新版，或者Python >= 3.6'
+# coding: utf-8
+
+import ctypes
+from time import sleep
+from functools import reduce
+import pyperclip
+import json
+
+WM_COMMAND, WM_SETTEXT, WM_GETTEXT, WM_KEYDOWN, WM_KEYUP = \
+    273,        12,         13,        256,        257           # 命令
+    
+F1,   F2,   F3,    F4,    F5,    F6 = \
+112,  113,  114,   115,   116,   117       # keyCode(按键代码)
+
+op = ctypes.windll.user32
+
+def keystroke(hCtrl, keyCode, param=0):   # 单击
+    op.PostMessageW(hCtrl, WM_KEYDOWN, keyCode, param)
+    op.PostMessageW(hCtrl, WM_KEYUP, keyCode, param)
+    
+class unity():
+    ''' 多账户交易集中处理 '''
+    
+    def __init__(self, main):
+    
+        keystroke(main, F6)    # 切换到双向委托
+        
+        sleep(0.1)    # 可调整区间值(0.01~0.5)
+        
+        self.buff = ctypes.create_unicode_buffer(32)
+        #            代码，价格，数量，买入，代码，价格，数量，卖出，全撤， 撤买， 撤卖
+        id_members = 1032, 1033, 1034, 1006, 1035, 1058, 1039, 1008, 30001, 30002, 30003, \
+                     32790, 1038, 1047, 2053, 30022, 1019   # 刷新，余额、表格、最后一笔、撤相同
+        self.path_custom = 1047, 200, 1047
+                     
+        self.two_way = reduce(op.GetDlgItem, (59648, 59649), main)
+        self.members = {i: op.GetDlgItem(self.two_way, i) for i in id_members}
+        self.custom = reduce(op.GetDlgItem, self.path_custom, self.two_way)
+        
+        # 获取登录账号
+        self.account = reduce(op.GetDlgItem, (59392, 0, 1711), main)
+        op.SendMessageW(self.account, WM_GETTEXT, 32, self.buff)
+        self.account = self.buff.value
+
+    def buy(self, symbol, price, qty):   # 买入(B)
+        # buy = order('b')
+        op.SendMessageW(self.members[1032], WM_SETTEXT, 0, symbol)
+        op.SendMessageW(self.members[1033], WM_SETTEXT, 0, price)
+        op.SendMessageW(self.members[1034], WM_SETTEXT, 0, qty)
+        op.PostMessageW(self.two_way, WM_COMMAND, 1006, self.members[1006])
+    
+    def sell(self, symbol, price, qty):    # 卖出(S)
+        # buy = order('s')
+        op.SendMessageW(self.members[1035], WM_SETTEXT, 0, symbol)
+        op.SendMessageW(self.members[1058], WM_SETTEXT, 0, price)
+        op.SendMessageW(self.members[1039], WM_SETTEXT, 0, qty)
+        op.PostMessageW(self.two_way, WM_COMMAND, 1008, self.members[1008])
+        op.GetDlgItemTextW(self.members[1019], self.buff, 32)
+        print(self.buff.value)
+
+    def refresh(self):    # 刷新(F5)
+        op.PostMessageW(self.two_way, WM_COMMAND, 32790, self.members[32790])
+        
+    def cancel(self, way=0):    # 撤销下单
+        pass
+        
+    def cancelAll(self):    # 全撤(Z)
+        op.PostMessageW(self.two_way, WM_COMMAND, 30001, self.members[30001])
+        
+    def cancelBuy(self):    # 撤买(X)
+        op.PostMessageW(self.two_way, WM_COMMAND, 30002, self.members[30002])
+        
+    def cancelSell(self):    # 撤卖(C)
+        op.PostMessageW(self.two_way, WM_COMMAND, 30003, self.members[30003])
+        
+    def cancelLast(self):    # 撤最后一笔，仅限华泰定制版有效
+        op.PostMessageW(self.two_way, WM_COMMAND, 2053, self.members[2053])
+        
+    def cancelSame(self):    # 撤相同代码，仅限华泰定制版
+        #op.PostMessageW(self.two_way, WM_COMMAND, 30022, self.members[30022])
+        pass
+        
+    def balance(self):    # 可用余额
+        op.SendMessageW(self.members[1038], WM_GETTEXT, 32, self.buff)
+        return self.buff.value
+        
+    def get_data(self, key='W'):
+        "将CVirtualGridCtrl|Custom<n>的数据复制到剪贴板，默认取持仓记录"
+        
+        keystroke(self.two_way, ord(key))    # 切换到持仓('W')、成交('E')、委托('R')
+        sleep(0.8)    # 等待券商的数据返回...
+        op.SendMessageW(self.custom, WM_COMMAND, 57634, 0, self.path_custom[-1])    # background mode
+        
+        return pyperclip.paste()
+        
+def finder():
+    """ 枚举所有已登录的交易端并将其实例化 """
+    
+    team = set()
+    buff = ctypes.create_unicode_buffer(32)
+    
+    @ctypes.WINFUNCTYPE(ctypes.wintypes.BOOL, ctypes.wintypes.HWND, ctypes.wintypes.LPARAM)
+    def check(hwnd, extra):
+        if op.IsWindowVisible(hwnd):
+            op.GetWindowTextW(hwnd, buff, 32)
+            if '交易系统' in buff.value:
+                team.add(hwnd)
+        return 1
+    op.EnumWindows(check, 0)
+    
+    return {unity(main) for main in team if team}
+    
+def to_dict(raw):
+    x = [row.split() for row in raw.splitlines()]
+    data = {}
+    for y in x[1:]:
+        data.update({y[0]: dict(zip(x[0], y))})
+    return data
+    
+if __name__ == '__main__':
+
+    myRegister = {'券商登录号': '自定义名称',   \
+                  '61770': '东方不败', \
+                  '45645464682532': '张无忌',   \
+                  '74378458454548': '钢铁侠',   \
+                  '84482525525038': '李寻欢',   \
+                  '20941552121212': '西门吹雪'}
+    
+    ret = finder()
+    
+    if ret:
+        # 如果没取到余额，尝试修改_init_函数里面sleep的值，或者查余额的id是不是变了。
+        trader = {myRegister[broker.account]: broker for broker in ret}    # 给账户一个易记的外号
+        trader1 = {broker.account[-3:]: broker.balance() for broker in ret} # 以登录号3位尾数作代号
+        profile = {solo: {"交易帐号": trader[solo].account, \
+                          "可用余额": trader[solo].balance()} \
+                   for solo in trader}
+                   
+        print(profile)
+        print(json.dumps(profile, indent=4, ensure_ascii=False, sort_keys=True))
+        
+        raw = trader['东方不败'].get_data()    # 只能“大写字母”，小写字母THS会崩溃，无语！
+        print(to_dict(raw))
+        
+        raw = trader['东方不败'].get_data('R')
+        print(json.dumps(to_dict(raw), indent=4, ensure_ascii=False))
+    
+    else: print("老板，没发现已登录的交易端！")
