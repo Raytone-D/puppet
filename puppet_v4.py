3d863dd56f286d1a4eccf62fab9ecc595f2a6feb
睿瞳深邃 <raytone@qq.com>
2017-11-02 23:30:53 +0800

    修正余额实时取值的问题。

--- puppet_v4.py
+++ puppet_v4.py
@@ -4,7 +4,7 @@
 """
 __author__ = "睿瞳深邃(https://github.com/Raytone-D)"
 __project__ = 'Puppet'
-__version__ = "0.4.21c"
+__version__ = "0.4.22"
 __license__ = 'MIT'
 
 # coding: utf-8
@@ -220,6 +220,8 @@ class Puppet:
 
     @property
     def balance(self):
+        self.switch(NODE['双向委托'])
+        time.sleep(0.5)
         print('可用余额: %s' % ('$'*8))
         op.SendMessageW(self.members['可用余额'], MSG['WM_GETTEXT'], 32, self.buff)
         return self.buff.value
