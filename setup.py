2a555f2d0b9f8f24991da2817c9c4c216141f82f
Raytone-D <raytone@qq.com>
2018-10-12 10:49:05 +0800

    feat(setup): add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+VERSION = '0.7.2'
+REQUIRED = ['pyperclip']
+REQUIRES_PYTHON = '>=3.4.0'
+
+
+setup(
+    name='puppet',
+    version=VERSION,
+    description=("一个用来交易A股的应用编程接口"),
+    license='MIT',
+    author='Raytone-D',
+    author_email='raytone@qq.com',
+    url='https://github.com/Raytone-D/puppet',
+    keywords="stock TraderApi Quant",
+    python_requires=REQUIRES_PYTHON,
+    install_requires=REQUIRED,
+    packages=['puppet'],
+    #test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
